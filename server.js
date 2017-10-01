5a4cbd0c605aea422861fa571330174630996435
Ashwin <aswinjeganath@gmail.com>
2017-10-01 22:26:07 +0530

    Updated server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.get('/', function(req, res) {
 
 app.get('/whoami', function(req, res){
   let obj = {}
-  obj.ipaddress = req.headers['x-forwarded-for']
+  obj.ipaddress = req.headers['x-forwarded-for'].split(',')[0]
         || req.connection.remoteAddress.replace(/^.*:/, '')
   obj.language = req.headers['accept-language'].replace(/,.*/, '')
   obj.software = new RegExp('\\((.*)\\)').exec(req.headers['user-agent'])[1]
